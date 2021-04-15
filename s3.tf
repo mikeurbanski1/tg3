04d26c2abdefeb8e2f426156836e149c5beb7016
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-04-15 13:42:12 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -12,17 +12,4 @@ resource "aws_s3_bucket" "xx" {
   }
 }
 
-resource "aws_s3_bucket" "xxx" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "xx"
-  acl           = "public-read"
-  force_destroy = true
-  tags = {
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-  }
-}
 
