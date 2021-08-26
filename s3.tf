4fdefde4d89820cf473fd310a86e4c3ad045faaa
Mike Urbanski <mike@bridgecrew.io>
2021-08-26 09:21:41 -0500

    a

--- s3.tf
+++ s3.tf
@@ -17,3 +17,23 @@ module "mod" {
 }
 
 
+module "dynamodb_table" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = "my-table"
+  hash_key = "id"
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
+
+  server_side_encryption_enabled = false
+}
