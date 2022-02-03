71b57691582e7499e7494ef6c8a6609b53062d0d
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-02-03 14:17:01 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -12,82 +12,82 @@ resource "aws_s3_bucket" "regular_bucket" {
   }
 }
 
-//resource "aws_something_else" "x" {
-//  hello = "hi"
-//}
-//
-//module "mod" {
-//  source = "./module"
-//  x = "abc"
-//}
-//
-//module "mod2" {
-//  source = "./module"
-//  x = "xyz"
-//}
-//
-//module "iam_policy" {
-//  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-//  version = "~> 4.3"
-//
-//  name        = "example"
-//  path        = "/"
-//  description = "My example policy"
-//
-//  policy = <<EOF
-//{
-//  "Version": "2012-10-17",
-//  "Statement": [
-//    {
-//      "Action": [
-//        "*"
-//      ],
-//      "Effect": "Allow",
-//      "Resource": "*"
-//    }
-//  ]
-//}
-//EOF
-//}
-//
-//module "dynamodb_table" {
-//  source   = "terraform-aws-modules/dynamodb-table/aws"
-//
-//  name     = "my-table"
-//  hash_key = "id"
-//
-//  attributes = [
-//    {
-//      name = "id"
-//      type = "N"
-//    }
-//  ]
-//
-//  tags = {
-//    Terraform   = "true"
-//    Environment = "staging"
-//  }
-//
-//  server_side_encryption_enabled = false
-//}
-//
-//module "dynamodb_table2" {
-//  source   = "terraform-aws-modules/dynamodb-table/aws"
-//
-//  name     = "my-table2"
-//  hash_key = "id2"
-//
-//  attributes = [
-//    {
-//      name = "id2"
-//      type = "N"
-//    }
-//  ]
-//
-//  tags = {
-//    Terraform   = "true2"
-//    Environment = "staging2"
-//  }
-//
-//  server_side_encryption_enabled = false
-//}
+resource "aws_something_else" "x" {
+  hello = "hi"
+}
+
+module "mod" {
+  source = "./module"
+  x = "abc"
+}
+
+module "mod2" {
+  source = "./module"
+  x = "xyz"
+}
+  
+module "iam_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "~> 4.3"
+
+  name        = "example"
+  path        = "/"
+  description = "My example policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+module "dynamodb_table" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = "my-table"
+  hash_key = "id"
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
+
+  server_side_encryption_enabled = false
+}
+
+module "dynamodb_table2" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = "my-table2"
+  hash_key = "id2"
+
+  attributes = [
+    {
+      name = "id2"
+      type = "N"
+    }
+  ]
+
+  tags = {
+    Terraform   = "true2"
+    Environment = "staging2"
+  }
+
+  server_side_encryption_enabled = false
+}
