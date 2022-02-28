9e5aa04ad0cd8e8429e113dcbb070273fc780ffb
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-02-28 12:58:09 -0600

    Update bad_bucket.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- bad_bucket.tf
+++ bad_bucket.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "bad_bucket" {
   bucket = "abc"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
