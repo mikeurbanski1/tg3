6c98e39801a6cd8ea25abd5ea026db99e8317d18
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-03-01 12:01:38 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -12,6 +12,23 @@ resource "aws_s3_bucket" "regular_bucket" {
   }
 }
 
+resource "aws_s3_bucket" "bad_bucket" {
+  #bridgecrew:skip=BC_AWS_GENERAL_26:justification
+  #bridgecrew:skip=CKV_AWS_145:justification
+  
+  bucket = "abc"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  versioning {
+    enabled = true
+  }
+}
+
 resource "aws_s3_bucket" "will_it_blend" {
   # bucket is public
   # bucket is not encrypted
