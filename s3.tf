b326628b8175e8e98e0cf638af6ac074d371b06b
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-03-10 09:13:58 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,48 +1,48 @@
-resource "aws_s3_bucket" "regular_bucket" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "xx"
-  acl           = "public-read"
-  force_destroy = true
-  tags = {
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-  }
-}
+# resource "aws_s3_bucket" "regular_bucket" {
+#   # bucket is public
+#   # bucket is not encrypted
+#   # bucket does not have access logs
+#   # bucket does not have versioning
+#   bucket        = "xx"
+#   acl           = "public-read"
+#   force_destroy = true
+#   tags = {
+#     Name        = "${local.resource_prefix.value}-data"
+#     Environment = local.resource_prefix.value
+#   }
+# }
 
-resource "aws_s3_bucket" "will_it_blend" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "xx"
-  acl           = "public-read"
-  force_destroy = true
-  tags = {
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-  }
-}
+# resource "aws_s3_bucket" "will_it_blend" {
+#   # bucket is public
+#   # bucket is not encrypted
+#   # bucket does not have access logs
+#   # bucket does not have versioning
+#   bucket        = "xx"
+#   acl           = "public-read"
+#   force_destroy = true
+#   tags = {
+#     Name        = "${local.resource_prefix.value}-data"
+#     Environment = local.resource_prefix.value
+#   }
+# }
 
-resource "aws_s3_bucket" "regular_bucket2" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "xx2"
-  acl           = "public-read"
-  force_destroy = true
-  tags = {
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-  }
-}
+# resource "aws_s3_bucket" "regular_bucket2" {
+#   # bucket is public
+#   # bucket is not encrypted
+#   # bucket does not have access logs
+#   # bucket does not have versioning
+#   bucket        = "xx2"
+#   acl           = "public-read"
+#   force_destroy = true
+#   tags = {
+#     Name        = "${local.resource_prefix.value}-data"
+#     Environment = local.resource_prefix.value
+#   }
+# }
 
-resource "aws_something_else" "x" {
-  hello = "hi"
-}
+# resource "aws_something_else" "x" {
+#   hello = "hi"
+# }
 
 module "mod" {
   source = "./module"
@@ -50,73 +50,73 @@ module "mod" {
   acl = "public-read"
 }
 
-module "mod2" {
-  source = "./module"
-  x = "xyz"
-}
+# module "mod2" {
+#   source = "./module"
+#   x = "xyz"
+# }
   
-module "iam_policy" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "~> 4.3"
+# module "iam_policy" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+#   version = "~> 4.3"
 
-  name        = "example"
-  path        = "/"
-  description = "My example policy"
+#   name        = "example"
+#   path        = "/"
+#   description = "My example policy"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "*"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-module "dynamodb_table" {
-  source   = "terraform-aws-modules/dynamodb-table/aws"
+# module "dynamodb_table" {
+#   source   = "terraform-aws-modules/dynamodb-table/aws"
 
-  name     = "my-table"
-  hash_key = "id"
+#   name     = "my-table"
+#   hash_key = "id"
 
-  attributes = [
-    {
-      name = "id"
-      type = "N"
-    }
-  ]
+#   attributes = [
+#     {
+#       name = "id"
+#       type = "N"
+#     }
+#   ]
 
-  tags = {
-    Terraform   = "true"
-    Environment = "staging"
-  }
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "staging"
+#   }
 
-  server_side_encryption_enabled = false
-}
+#   server_side_encryption_enabled = false
+# }
 
-module "dynamodb_table2" {
-  source   = "terraform-aws-modules/dynamodb-table/aws"
+# module "dynamodb_table2" {
+#   source   = "terraform-aws-modules/dynamodb-table/aws"
 
-  name     = "my-table2"
-  hash_key = "id2"
+#   name     = "my-table2"
+#   hash_key = "id2"
 
-  attributes = [
-    {
-      name = "id2"
-      type = "N"
-    }
-  ]
+#   attributes = [
+#     {
+#       name = "id2"
+#       type = "N"
+#     }
+#   ]
 
-  tags = {
-    Terraform   = "true2"
-    Environment = "staging2"
-  }
+#   tags = {
+#     Terraform   = "true2"
+#     Environment = "staging2"
+#   }
 
-  server_side_encryption_enabled = false
-}
+#   server_side_encryption_enabled = false
+# }
