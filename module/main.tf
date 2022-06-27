728d3160671dd5cc2c172385a40a255767a36937
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-06-27 14:45:01 -0500

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -21,12 +21,3 @@ variable "acl" {
   
 }
 
-variable "enc" {
-  default = false
-}
-
-resource "aws_ebs_volume" "ebs" {
-  availability_zone = "us-west-2a"
-  size              = 40
-  encrypted = var.enc
-}
