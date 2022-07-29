31f8c5603eb5a6a8e2702822b53428eb9d2ffb0b
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2022-07-29 12:19:37 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,6 +2,11 @@ resource "aws_s3_bucket" "default" {
   count         = local.enabled ? 1 : 0
   bucket        = local.bucket_name
   force_destroy = var.force_destroy
+  
+  tags = {
+    owner = "mike"
+    test = "no you test"
+  }
 
   dynamic "object_lock_configuration" {
     for_each = var.object_lock_configuration != null ? [1] : []
